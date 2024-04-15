e2203165a5c413c089131135016b33664968b159
DanteNorlingTE21 <dante.norling@elev.ga.ntig.se>
2024-04-15 09:49:33 +0200

    p

--- init.js
+++ init.js
@@ -1,1 +1,2 @@
-document.getElementsByTagName("header")[0].innerHTML = "<h1>Chef Tingeling</h1> <nav> <ul> <li><a href='index.html'>Home</a></li> <li><a href='about.html'>About</a></li> <li><a href='booking.html'>Booking</a></li> </ul> </nav>";
+document.getElementsByTagName("header")[0].innerHTML = "<h1>Chef Tingeling</h1> <nav> <ul> <li><a href='index.html'>Home</a></li><li><a href='booking.html'>Booking</a></li> <li><a href='about.html'>About</a></li>  </ul> </nav>";
+document.getElementsByTagName("footer")[0].innerHTML = "<p>© 2021 Chef Tingeling</p>";
